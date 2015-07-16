0ee76debb1ce4bb8c9131f029a40086381e2d286
anotherworkman <anotherworkman@gmail.com>
2015-07-16 18:34:03 +0300

    header added

--- index.js
+++ index.js
@@ -6,23 +6,56 @@ define([
     function node(text, nested) {
         return { data: { text: text }, nested: nested ? nested : [] };
     }
+    var docData = node('Деревья', [
+        node('Хвойные', [
+            node('Сосновые', [
+                node('Сосна'),
+                node('Ель'),
+                node('Кедр'),
+                node('Лиственница'),
+                node('Пихта')
+            ]),
+            node('Кипарисовые', [
+                node('Кипарис'),
+                node('Можжевельник'),
+                node('Секвойя')
 
-    var docData = node('Хвойные', [
-        node('Сосновые', [
-            node('Ель'),
-            node('Сосна'),
-            node('Кедр'),
-            node('Лиственница'),
-            node('Пихта'),
-            node('Тсуга')
+            ])
         ]),
-        node('Кипарисовые', [
-            node('Можжевельник'),
-            node('Секвойя'),
-            node('Кипарис'),
-            node('Туя')
+        node('Плодовые', [
+            node('Розовые', [
+                node('Яблоня'),
+                node('Груша'),
+                node('Айва'),
+                node('Слива', [
+                    node('Слива домашняя'),
+                    node('Алыча'),
+                    node('Вишня'),
+                    node('Черешня'),
+                    node('Абрикос'),
+                    node('Персик'),
+                    node('Миндаль')
+                ])
+            ])
         ])
     ]);
+    //
+    //var docData = node('Хвойные', [
+    //    node('Сосновые', [
+    //        node('Ель'),
+    //        node('Сосна'),
+    //        node('Кедр'),
+    //        node('Лиственница'),
+    //        node('Пихта'),
+    //        node('Тсуга')
+    //    ]),
+    //    node('Кипарисовые', [
+    //        node('Можжевельник'),
+    //        node('Секвойя'),
+    //        node('Кипарис'),
+    //        node('Туя')
+    //    ])
+    //]);
 
     var nestedTextDoc = NestedText.createDocument(docData);
     var editorBox = document.querySelector('.demo__editor-box');
@@ -35,6 +68,9 @@ define([
     editorBox.parentElement.addEventListener('click', function(e) {
         editorBox.firstElementChild.focus();
     });
+
+    document.documentElement.scrollTop = document.body.scrollTop = 0;
+    // todo сделать, чтобы редактор принимал флаг focusOnInit
 
     // todo resolve platform for keymap
 
